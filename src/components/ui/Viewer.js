6e2a848238246a84bc044c7782825321236d3b60
pauloricca <pauloricca@gmail.com>
2021-07-16 01:53:59 +0100

    Sortable views

--- src/components/ui/Viewer.js
+++ src/components/ui/Viewer.js
@@ -1,6 +1,7 @@
 import VTextBox from "../views/VTextBox";
 import VImageWithPadding from "../views/VImageWithPadding";
 import VImageCarousel from "../views/VImageCarousel";
+import { ReactSortable } from "react-sortablejs";
 import './Viewer.scss';
 
 
@@ -12,21 +13,23 @@ function Viewer(props) {
         props.onAddNew(event.target.value);
         event.target.value = "";
     }
-    
-    const views = props.views.map( (view, vi) => {
-        const onChange = (fieldName, value) => props.onChange(vi, fieldName, value);
-        const onRemove = () => props.onRemove(vi);
-        switch(view.moduleType) {
-            case 'VTextBox': return <VTextBox key={vi} attributes={view.attributes} mode={props.mode} onChange={onChange} onRemove={onRemove}/>;
-            case 'VImageWithPadding': return <VImageWithPadding key={vi} attributes={view.attributes} mode={props.mode} onChange={onChange} onRemove={onRemove}/>;
-            case 'VImageCarousel': return <VImageCarousel key={vi} attributes={view.attributes} mode={props.mode} onChange={onChange} onRemove={onRemove}/>;
-            default: return null;
-        };
-    });
 
     return (
         <div className={'Viewer ' + props.mode}>
-            {views}
+
+            <ReactSortable list={props.views} setList={props.onNewData}>
+                {props.views.map( (view, vi) => {
+                    const onChange = (fieldName, value) => props.onChange(vi, fieldName, value);
+                    const onRemove = () => props.onRemove(vi);
+                    switch(view.moduleType) {
+                        case 'VTextBox': return <VTextBox key={vi} attributes={view.attributes} mode={props.mode} onChange={onChange} onRemove={onRemove}/>;
+                        case 'VImageWithPadding': return <VImageWithPadding key={vi} attributes={view.attributes} mode={props.mode} onChange={onChange} onRemove={onRemove}/>;
+                        case 'VImageCarousel': return <VImageCarousel key={vi} attributes={view.attributes} mode={props.mode} onChange={onChange} onRemove={onRemove}/>;
+                        default: return null;
+                    };
+                })}
+            </ReactSortable>
+
             {props.mode==="edit"?
                 <div className="ui-columns">
                     <select onChange={addNew} value="" className="add-new-module">
@@ -35,9 +38,10 @@ function Viewer(props) {
                         <option value="VImageWithPadding">Image</option>
                         <option value="VImageCarousel">Carousel</option>
                     </select>
-                    <button>Save</button>
+                    <button onClick={props.onSave}>Save</button>
                 </div>
             :null}
+
         </div>
     );
   }
